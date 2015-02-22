d48f39da1d7745fd6f235a97924adfb9311efbb9
卜错 <baiming.org@gmail.com>
2015-02-22 16:46:02 +0800

    Initial Commit

---
+++ longPressGuesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  longPressGuesture
+//
+//  Created by bbu on 2/22/15.
+//  Copyright (c) 2015 bbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ longPressGuesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  longPressGuesture
+//
+//  Created by bbu on 2/22/15.
+//  Copyright (c) 2015 bbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
